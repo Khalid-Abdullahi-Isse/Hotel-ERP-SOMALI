49d6da769675122001c8a142e7b3293745e35611
khalid abdullahi isse <khalidabdullahiesse@gmail.com>
2026-08-25 07:56:05 +0300

    Remove Next.js build files from tracking

--- frontend/frontend/.next/dev/static/chunks/_1q4jyjr._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__01uukdd._.css","static/chunks/node_modules_0tttjdl._.js","static/chunks/src_components_providers_query-provider_tsx_1rem7_b._.js","static/chunks/node_modules_1v34z5x._.js","static/chunks/node_modules_0fk3824._.js","static/chunks/src_0btdff0._.js","static/chunks/src_1ninnpo._.js","static/chunks/node_modules_15c2dzy._.js","static/chunks/_04wt-v9._.js","static/chunks/node_modules_1gjjrfc._.js","static/chunks/src_components_0yy1mdj._.js"],
-    source: "entry"
-});
